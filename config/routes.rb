ff30a5ac86e8dbfc5b2ed160d2147609cfed0318
Andi Setianto <andi.w.setianto@gmail.com>
2016-04-20 01:40:48 +0000

    index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Web::Application.routes.draw do
+
+root 'comments#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
